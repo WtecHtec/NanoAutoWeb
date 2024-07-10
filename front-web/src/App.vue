df68a871ba8f83a1544fc583d8035880eeaf3a20
sr7 <504105925@qq.com>
2024-07-10 22:46:50 +0800

    feat:antvg6

--- front-web/src/App.vue
+++ front-web/src/App.vue
@@ -1,22 +1,13 @@
 <script setup lang="ts">
-	import BaseUI from './components/Base/Index.vue'
+	// import BaseUI from './components/Base/Index.vue'
+
+	import MindMapUI from './components/MindMap/Index.vue'
 </script>
 
 <template>
-  <BaseUI  />
+  <MindMapUI  />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
